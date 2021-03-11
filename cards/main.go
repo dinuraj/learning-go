a4fcf22b757baf3e88930ac131fac9e24a00ae44
dinuraj <dinuraj@gmail.com>
2021-03-11 13:41:23 +0530

    HW

---
+++ cards/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	cards := deck{"Ace of Diamonds", newCard()}
+
+	cards = append(cards, "Six of Spades")
+
+	fmt.Println(cards)
+
+	cards.print()
+
+}
+
+func newCard() string {
+	return "Five of Diamonds"
+}
